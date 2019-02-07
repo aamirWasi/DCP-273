5760c840c5a64db6a98afcfd03c865deccc2cefc
aamir <wasi3aamir@gmail.com>
2019-02-07 07:07:17 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                if (line.Contains('r'))
+                {
+                    Console.WriteLine("Not vely easy");
+                }
+                else
+                {
+                    Console.WriteLine("Vely easy");
+                }
+            }
+        }
+    }
+}
